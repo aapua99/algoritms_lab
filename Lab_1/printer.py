a2c6b62129352cc5b77124933fc1867667719720
aapua99 <32804535+aapua99@users.noreply.github.com>
2018-09-13 09:04:17 +0300

    add lab_1

---
+++ Lab_1/printer.py
@@ -0,0 +1,7 @@
+class Printer:
+    def __init__(self, name, speed, price):
+        self.name=name
+        self.speed=speed
+        self.price=price
+    def print_printer(self):
+        print("Name: "+str(self.name)+" , Price "+str(self.price)+" , Speed "+str(+self.speed))
